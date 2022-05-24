f844956eb2d61b048a6359a9985138b03c37533f
Ilnaz Valiev <ilvaliev@microsoft.com>
2022-05-25 01:56:39 +0300

    c

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 const WheelApp = React.lazy(() => import('wheel/App'));
 
 function App() {
+    const [show, setShow] = useState(false);
+
     return (
         <>
-            <React.Suspense fallback={<>Loading . . .</>}>
-                <WheelApp />
-            </React.Suspense>
+            App:
+            <button onClick={() => setShow((cur) => !cur)}>
+                Toggle (loads app)
+            </button>
+            {show && (
+                <React.Suspense fallback={<>Loading . . .</>}>
+                    <WheelApp />
+                </React.Suspense>
+            )}
         </>
     );
 }
