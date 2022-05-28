7de9a6006566fdb006e665d475940397f314b071
Ilnaz Valiev <ilvaliev@microsoft.com>
2022-05-29 01:54:59 +0300

    loader

--- src/Background/Background.tsx
+++ src/Background/Background.tsx
@@ -1,14 +1,29 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useEffect, useMemo, useState } from 'react';
 import { Colors } from '../constants';
 
+const imgUrl = 'https://i.redd.it/ozu901un81191.jpg';
+
 export function Background({ children }: PropsWithChildren<{}>): JSX.Element {
-    return (
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const image = new Image();
+        image.src = imgUrl;
+        image.onload = (ev) => {
+            setLoading(false);
+        };
+    }, []);
+
+    return loading ? (
+        <div className="root">
+            <div className="container">
+                <div className="loader"></div>
+            </div>
+        </div>
+    ) : (
         <div
             style={{
-                backgroundImage: 'url(https://i.redd.it/ozu901un81191.jpg)',
-                backgroundSize: 'cover',
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center center',
+                background: `url("${imgUrl}") center center / cover no-repeat gray`,
                 height: `100vh`,
                 width: `100wh`,
             }}
